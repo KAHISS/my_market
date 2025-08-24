c0b45f5b1ac0707e36a89948815a8f9e250d3d8a
Vinícius <viniciusandradebc@gmail.com>
2025-08-24 20:15:28 -0300

    feat: adicionando perfil de usuário

--- employee/urls.py
+++ employee/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'employee'
 
 urlpatterns = [
-    path('pos/', views.pos, name='pos'),
+    path('profile/', views.profile, name='pos'),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def pos(request):
-    return render(request, 'employee/pages/pos.html')
+def profile(request):
+    return render(request, 'employee/pages/profile.html')
