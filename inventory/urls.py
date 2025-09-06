8a3194e474c70bd6e01a721177f7a63adbdc70a8
Kaique Silva Sousa <ksilvasousa1@gmail.com>
2025-09-05 21:43:39 -0300

    feat: ajeitando a navegação do estoque

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'inventory'
 
 urlpatterns = [
-    path('', views.index, name='storage'),
+    path('', views.home, name='storage'),
+    path('product/<int:id>', views.product, name='product'),
+    path('add-product-view', views.addProduct, name='addProductView'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -4,7 +4,19 @@ from .models import Product
 # Create your views here.
 
 
-def index(request):
+def home(request):
     products = Product.objects.all()
     return render(request, 'inventory/pages/storage.html', context={
-        'products': products})
+        'products': products}
+    )
+
+
+def product(request, id):
+    product = Product.objects.filter(id=id).first()
+    return render(request, 'inventory/pages/product-view.html', context={
+        'product': product}
+    )
+
+
+def addProduct(request):
+    return render(request, 'inventory/pages/addNewProduct-view.html')
