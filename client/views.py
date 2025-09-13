8bcebd5481e9bc39eeea34b2457ed60da7ee4d03
Vinícius <viniciusandradebc@gmail.com>
2025-09-13 15:40:50 -0300

    feat: fazendo migrations

--- client/views.py
+++ client/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 def profile(request):
     return render(request, 'client/pages/profile.html')
 
+def profile(request, id): 
+    client = Client.objects.filter(id=id).first()
+    return render(request, 'client/pages/profile.html', context= {
+                'client': client})
+
 def management(request):
-    return render(request, 'client/pages/clients-management.html')
+    clients = Client.objects.all()
+    return render(request, 'client/pages/clients-management.html', context={
+        'clients': clients})
 
 def add_client(request):
     return render(request, 'client/pages/add-new-client.html')
